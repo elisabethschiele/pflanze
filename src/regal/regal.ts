bc448a08418fa317c9938f75a093f01aad4301da
elisabethschiele <elisabethschiele@gmail.com>
2018-08-11 19:02:07 +0200

    regal wieder da

---
+++ src/regal/regal.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-regal',
+  templateUrl: 'regal.html'
+})
+export class RegalPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
