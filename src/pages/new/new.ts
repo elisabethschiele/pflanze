cde487055f50f6fb80e5f9173730d54c85dcff9a
elisabethschiele <elisabethschiele@gmail.com>
2018-08-11 18:54:48 +0200

    sample pics, block view

--- src/pages/new/new.ts
+++ src/pages/new/new.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { Data } from '../../providers/data/data';
+import { RestProvider } from '../../providers/rest/rest';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-new',
@@ -7,8 +10,36 @@ import { NavController } from 'ionic-angular';
 })
 export class NewPage {
 
-  constructor(public navCtrl: NavController) {
+  searchTerm: string = '';
+  items: any;
+  filteredItems: any;
 
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+    this.getPflanzen();
+  }
+
+  getPflanzen() {
+
+    this.restProvider.getData().then(data => {
+      this.items = data;
+      console.log(this.items);
+      this.setFilteredItems();
+    });
+
+  }
+
+  setFilteredItems() {
+
+      this.filteredItems = this.filterItems(this.searchTerm);
+
+  }
+
+
+  filterItems(searchTerm){
+
+    return this.items.filter((item) => {
+      return item.Name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
   }
 
 }
